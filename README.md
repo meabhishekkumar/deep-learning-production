b53a12c3b48c695852d9197c0676f8df5ff6eb9d
aku340 <akumar340@sapient.com>
2019-03-10 14:22:08 +0530

    chaging the tensorflow serving tutorial

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ We will be using several custom codelab tutorials created for this session.
 
 here are the links to the tutorials 
 
-1. Lab1 : [Basics of Tensorflow Serving](https://meabhishekkumar.github.io/deep-learning-production/codelabs/01-basic-tensorflow-serving-tutorial/index.html)
-2. Basics of Kubeflow 
+1. Lab1 : [Deploying Deep Learning Model using Tensorflow Serving](https://meabhishekkumar.github.io/deep-learning-production/codelabs/01-basic-tensorflow-serving-tutorial/index.html)
+2. Lab2 : [End-To-End Machine Learning Workflow using Kubeflow] 
--- code/01-basic-tensorflow-serving/model.py
+++ code/01-basic-tensorflow-serving/model.py
@@ -2,31 +2,49 @@ import tensorflow as tf
 import numpy as np 
 import os 
 from datetime import datetime
+import shutil
+
+tf.app.flags.DEFINE_integer('training_iteration', 500,'number of training iterations.')
+FLAGS = tf.app.flags.FLAGS
 
 
-### dataset : input and output
-xs = np.array([-1,0,1,2,3,4], dtype=np.float)
-ys = np.array([-3,-1,1,3,5,7], dtype=np.float)
+def main(_):
+    ### dataset : input and output
+    xs = np.array([-1,0,1,2,3,4], dtype=np.float)
+    ys = np.array([-3,-1,1,3,5,7], dtype=np.float)
 
-### create simple model 
-model = tf.keras.Sequential([tf.keras.layers.Dense(units=1, input_shape=[1])])
-model.compile(optimizer='sgd',loss='mean_squared_error')
+    ### create simple model 
+    model = tf.keras.Sequential([tf.keras.layers.Dense(units=1, input_shape=[1])])
+    model.compile(optimizer='sgd',loss='mean_squared_error')
 
-### train the model
-model.fit(xs, ys, epochs=500)
+    ### train the model
+    model.fit(xs, ys, epochs=FLAGS.training_iteration)
+    print("trained the model")
 
-### predict using model 
-print(model.predict([10.0]))
+    ### predict using model 
+    print("predict using model")
+    print(model.predict([10.0]))
 
-### export the model
-now = datetime.now() 
-model_folder = now.strftime("%m%d%Y%H%M%S")
-export_path = os.path.join(os.path.curdir,"output",model_folder)
+    ### export the model
+    now = datetime.now() 
+    model_folder = now.strftime("%m%d%Y%H%M%S")
+    # clean the output foler
+    shutil.rmtree(os.path.join(os.path.curdir,"output"))
+    # create output folder
+    os.makedirs(os.path.join(os.path.curdir,"output"))
+    # full export path
+    export_path = os.path.join(os.path.curdir,"output",model_folder)
 
-with tf.keras.backend.get_session() as sess:
-    tf.saved_model.simple_save(
-        sess,
-        export_path,
-        inputs={'input_number': model.input},
-        outputs={t.name:t for t in model.outputs})
+    # export the model
+    with tf.keras.backend.get_session() as sess:
+        tf.saved_model.simple_save(
+            sess,
+            export_path,
+            inputs={'input_number': model.input},
+            outputs={t.name:t for t in model.outputs})
+    print("exported the model")
+    pass
+        
+if __name__ == '__main__':
+    tf.app.run()
 
